9e19c1e6df5d19bd09d9874c4c6f63f8e9fbcf75
eason <qq912100012@gmail.com>
2025-10-12 08:31:06 -0400

    init

--- PaperSearch/agent.py
+++
@@ -1,9 +0,0 @@
-from google.adk.agents.llm_agent import Agent
-from google.adk.models.lite_llm import LiteLlm
-
-root_agent = Agent(
-    model=LiteLlm(model="deepseek/deepseek-chat",api_key="sk-0ceb4613cbe547249a436c4f60b80dd4"),
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
-)
